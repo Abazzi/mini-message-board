210a4f36a44363e67607c887c8924186eeaf9939
Adam Bazzi <adam@adambazzi.ca>
2025-07-18 13:12:10 -0400

    index now takes an array of message objects and renders them in a div

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const app = express();
 const PORT = 3000;
 const assetsPath = path.join(__dirname, 'public');
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+
 // Linking and enabling the use of EJS files
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -11,7 +25,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(assetsPath));
 
 app.get('/', (req, res) => {
-  res.render('index', { header: "Mini Message Board" });
+  res.render('index', { header: "Mini Message Board", messages: messages });
 })
 
 app.listen(PORT, () => {
