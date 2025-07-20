d93bfbb01cd017dc7b215cf43960adbe7aed0ede
Adam Bazzi <adam@adambazzi.ca>
2025-07-20 10:46:06 -0400

    finished project

--- app.js
+++ app.js
@@ -6,29 +6,42 @@ const assetsPath = path.join(__dirname, 'public');
 
 const messages = [
   {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
   },
   {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
 ];
-
 
 // Linking and enabling the use of EJS files
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(assetsPath));
 
 app.get('/', (req, res) => {
-  res.render('index', { header: "Mini Message Board", messages: messages });
-})
+  res.render('index', { header: 'Mini Message Board', messages: messages });
+});
+
+app.get('/new', (req, res) => {
+  res.render('form', { header: 'New Message' });
+});
+
+app.post('/new', (req, res) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  });
+  res.redirect('/');
+});
 
 app.listen(PORT, () => {
-  console.log("Mini Message Board Running")
-})
-
+  console.log('Mini Message Board Running');
+});
